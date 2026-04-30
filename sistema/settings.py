e91e21c0cbd9368965b7653b2320fc8cbc5a19da
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-04-29 21:36:51 -0400

    Fix: Django version, remove redundant config and clean nixpacks

--- sistema/settings.py
+++ sistema/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*'] # Para debug en Railway, luego puedes restringirlo
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
