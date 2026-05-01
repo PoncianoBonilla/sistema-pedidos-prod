69a54884abfca846d0a706597bb31a6568d98130
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-05-01 17:32:14 -0400

    sqlite3

--- sistema/settings.py
+++ sistema/settings.py
@@ -104,10 +104,11 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # ======================
 # DATABASE
 # ======================
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+    "default": dj_database_url.parse(
+        DATABASE_URL if DATABASE_URL else "sqlite:///db.sqlite3"
     )
 }
 # ======================
