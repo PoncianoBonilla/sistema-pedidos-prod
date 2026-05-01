25c5056b9eb55908fa34a9303ed9d5afba7745a9
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-05-01 15:55:30 -0400

    fix static urls

--- sistema/urls.py
+++ sistema/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.shortcuts import render
 
+# 👇 IMPORTANTE para static
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 def index(request):
     return render(request, 'index.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +18,9 @@ urlpatterns = [
     path('api/productos/', include('producto.urls')),
     path('api/pedidos/', include('pedido.urls')),
 
-    # 👇 IMPORTANTE: excluir static
+    # 👇 MUY IMPORTANTE: excluir static correctamente
     re_path(r'^(?!static/).*$', index),
 ]
+
+# 👇 ESTO ES CLAVE PARA QUE FUNCIONEN LOS STATIC
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
