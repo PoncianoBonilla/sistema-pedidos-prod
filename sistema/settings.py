8d618454e2b885bdd7f28c74a661b2b19bb43f5c
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-05-01 17:21:32 -0400

    sqlite

--- sistema/settings.py
+++ sistema/settings.py
@@ -104,17 +104,12 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # ======================
 # DATABASE
 # ======================
-if os.getenv('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
+}
 # ======================
 # PASSWORDS
 # ======================
