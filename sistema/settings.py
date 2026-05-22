b2ba4a99b22c75c98ee330d0ac3b839baaec40fa
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-05-21 21:06:51 -0400

    fix database railway

--- sistema/settings.py
+++ sistema/settings.py
@@ -116,11 +116,10 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # ======================
 # DATABASE
 # ======================
-DATABASE_URL = os.environ.get("DATABASE_URL") or "sqlite:///db.sqlite3"
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        DATABASE_URL,
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
