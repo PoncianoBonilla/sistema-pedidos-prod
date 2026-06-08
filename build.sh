9650f9ef9bcf7995856d81b6732c089a466b909d
Ponciano Bonilla Catari <poncibonilla@gmail.com>
2026-06-07 22:14:47 -0400

    Fix: encoding de requirements y permisos de build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-# exit on error
+# Salir inmediatamente si un comando falla
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recopilar archivos estáticos
 python manage.py collectstatic --no-input
+
+# ¡ESTA LÍNEA HACE LA MAGIA!
 python manage.py migrate
